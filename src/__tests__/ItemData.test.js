67641996ac1e4a7707dae83819457ca44cd1544f
Brandon Lim <brandonlimchi@gmail.com>
2022-01-19 23:17:13 -0800

    refactor ItemData to remove the parsing of item history data and move that to its own component

--- src/__tests__/ItemData.test.js
+++ src/__tests__/ItemData.test.js
@@ -5,151 +5,18 @@ import axios from 'axios';
 import ItemData from '../components/ItemData';
 
 jest.mock('axios');
+afterEach(cleanup);
 
-describe('fetching and loading item data into graphs', () => {
-  afterEach(cleanup);
-
-  it('fetches and displays the item sale data when item is hq-able and stackable', async () => {
-    axios.get.mockResolvedValue({
-      data: { 
-        CanBeHq: 1,
-        StackSize: 99,
-        entries: [
-          {
-            quantity: 5,
-            pricePerUnit: 1234,
-            timestamp: 1642447903
-          },
-          {
-            quantity: 93,
-            pricePerUnit: 222,
-            timestamp: 1642447800
-          },
-          {
-            quantity: 34,
-            pricePerUnit: 1,
-            timestamp: 1642447111
-          },
-        ]
-      }
-    });
-  
-    const { queryByTestId } = render(<ItemData />);
-    await waitFor(() => {
-      expect(queryByTestId('sales-charts')).toBeInTheDocument();
-    });
-    expect(axios.get).toHaveBeenCalledTimes(2);
-    expect(queryByTestId('nq-sales-history')).toBeInTheDocument();
-    expect(queryByTestId('hq-sales-history')).toBeInTheDocument();
-    expect(queryByTestId('nq-ppu-history')).toBeInTheDocument();
-    expect(queryByTestId('hq-ppu-history')).toBeInTheDocument();
+it('fetches item property data', async () => {
+  axios.get.mockResolvedValue({
+    data: { 
+      CanBeHq: 1,
+      StackSize: 99
+    }
   });
 
-  it('fetches and displays the item sale data when item is hq-able but not stackable', async () => {
-    axios.get.mockResolvedValue({
-      data: { 
-        CanBeHq: 1,
-        StackSize: 1,
-        entries: [
-          {
-            quantity: 5,
-            pricePerUnit: 1234,
-            timestamp: 1642447903
-          },
-          {
-            quantity: 93,
-            pricePerUnit: 222,
-            timestamp: 1642447800
-          },
-          {
-            quantity: 34,
-            pricePerUnit: 1,
-            timestamp: 1642447111
-          },
-        ]
-      }
-    });
-  
-    const { queryByTestId } = render(<ItemData />);
-    await waitFor(() => {
-      expect(queryByTestId('sales-charts')).toBeInTheDocument();
-    });
-    expect(axios.get).toHaveBeenCalledTimes(2);
-    expect(queryByTestId('nq-sales-history')).toBeNull();
-    expect(queryByTestId('hq-sales-history')).toBeNull();
-    expect(queryByTestId('nq-ppu-history')).toBeInTheDocument();
-    expect(queryByTestId('hq-ppu-history')).toBeInTheDocument();
-  });
-
-  it('fetches and displays the item sale data when item is not hq-able and stackable', async () => {
-    axios.get.mockResolvedValue({
-      data: { 
-        CanBeHq: 0,
-        StackSize: 99,
-        entries: [
-          {
-            quantity: 5,
-            pricePerUnit: 1234,
-            timestamp: 1642447903
-          },
-          {
-            quantity: 93,
-            pricePerUnit: 222,
-            timestamp: 1642447800
-          },
-          {
-            quantity: 34,
-            pricePerUnit: 1,
-            timestamp: 1642447111
-          },
-        ]
-      }
-    });
-  
-    const { queryByTestId } = render(<ItemData />);
-    await waitFor(() => {
-      expect(queryByTestId('sales-charts')).toBeInTheDocument();
-    });
-    expect(axios.get).toHaveBeenCalledTimes(2);
-    expect(queryByTestId('nq-sales-history')).toBeInTheDocument();
-    expect(queryByTestId('hq-sales-history')).toBeNull();
-    expect(queryByTestId('nq-ppu-history')).toBeInTheDocument();
-    expect(queryByTestId('hq-ppu-history')).toBeNull();
-  });
-
-  it('fetches and displays the item sale data when item is not hq-able and not stackable', async () => {
-    axios.get.mockResolvedValue({
-      data: { 
-        CanBeHq: 0,
-        StackSize: 1,
-        entries: [
-          {
-            quantity: 5,
-            pricePerUnit: 1234,
-            timestamp: 1642447903
-          },
-          {
-            quantity: 93,
-            pricePerUnit: 222,
-            timestamp: 1642447800
-          },
-          {
-            quantity: 34,
-            pricePerUnit: 1,
-            timestamp: 1642447111
-          },
-        ]
-      }
-    });
-  
-    const { queryByTestId } = render(<ItemData />);
-    await waitFor(() => {
-      expect(queryByTestId('sales-charts')).toBeInTheDocument();
-    });
-    expect(axios.get).toHaveBeenCalledTimes(2);
-    expect(queryByTestId('nq-sales-history')).toBeNull();
-    expect(queryByTestId('hq-sales-history')).toBeNull();
-    expect(queryByTestId('nq-ppu-history')).toBeInTheDocument();
-    expect(queryByTestId('hq-ppu-history')).toBeNull();
+  const { queryByTestId } = render(<ItemData />);
+  await waitFor(() => {
+    expect(queryByTestId('item-data')).toBeInTheDocument();
   });
 });
--- src/components/ItemData.js
+++ src/components/ItemData.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import SalesHistoryChart from "./SaleHistoryChart";
-import PricePerUnit from "./PricePerUnitChart";
-import secondsToDate from "../helpers/secondsToDate";
 import axios from "axios";
+import ItemCharts from "./ItemCharts";
 
 const ItemData = (props) => {
-  const [itemData, setItemData] = useState(null);
   const [itemProperties, setItemProperties] = useState(null);
   const { itemID, world } = useParams();
-  const itemMarketURL = `https://universalis.app/api/history/${world}/${itemID}?entries=500&statsWithin=604800000&entriesWithin=604800`;
   const itemInfoURL = `https://xivapi.com/Item/${itemID}`;
 
   useEffect(() => {
@@ -27,84 +23,20 @@ const ItemData = (props) => {
     });
   }, []);
 
-  useEffect(() => {
-    if(itemProperties) {
-      axios.get(itemMarketURL)
-      .then((response) => {
-        console.log('Response obtained');
-        parseData(response.data);
-      })
-      .catch((error) => {
-        console.log('something went wrong: ' + error);
-      });
-    }
-  }, [itemProperties]);
-
-  const parseData = (data) => {
-    const nqHistory = { labels: [], quantitySold: [], pricePerQuantity: [] };
-    const hqHistory = { labels: [], quantitySold: [], pricePerQuantity: [] };
-    const nqHistoryPPU = { labels: [], pricePerUnit: [] };
-    const hqHistoryPPU = { labels: [], pricePerUnit: [] };
-
-    data.entries.forEach((entry) => {
-      const date = secondsToDate(entry.timestamp);
-      if(entry.hq) {
-        hqHistory.labels.push(date);
-        hqHistory.quantitySold.push(entry.quantity);
-        hqHistory.pricePerQuantity.push(entry.pricePerUnit);
-        hqHistoryPPU.labels.push(date);
-        hqHistoryPPU.pricePerUnit.push(entry.pricePerUnit / entry.quantity);
-      }
-      else {
-        nqHistory.labels.push(date);
-        nqHistory.quantitySold.push(entry.quantity);
-        nqHistory.pricePerQuantity.push(entry.pricePerUnit);
-        nqHistoryPPU.labels.push(date);
-        nqHistoryPPU.pricePerUnit.push(entry.pricePerUnit / entry.quantity);
-      }
-    });
-  
-    setItemData({
-      nqHistory, hqHistory, nqHistoryPPU, hqHistoryPPU
-    });
-  };
-
   return (
-    <div id="item-data">
+    <div id="item-data" data-testid="item-data">
       {
-        itemData ?
-          <section id="sales-charts" data-testid="sales-charts">
-            <div id="nq-sales">
-              {
-                itemProperties.stackSize > 1 &&
-                  <div className="sales-history-chart" data-testid="nq-sales-history">
-                    <SalesHistoryChart data={itemData.nqHistory} />
-                  </div>
-              }
-              <div className="price-per-unit-chart" data-testid="nq-ppu-history">
-                <PricePerUnit data={itemData.nqHistoryPPU} />
-              </div>
-            </div>
-            {
-              itemProperties.canBeHq &&
-                <div id="hq-sales">
-                  {
-                    itemProperties.stackSize > 1 &&
-                      <div className="sales-history-chart" data-testid="hq-sales-history">
-                        <SalesHistoryChart data={itemData.hqHistory} />
-                      </div>
-                  }
-                  <div className="price-per-unit-chart" data-testid="hq-ppu-history">
-                    <PricePerUnit data={itemData.hqHistoryPPU} />
-                  </div>
-                </div>
-            }
-          </section>
+        itemProperties ?
+          <ItemCharts 
+            world={world} 
+            itemID={itemID} 
+            itemProperties={itemProperties} 
+          />
         :
-          <div>Loading...</div>
+          <div className="loading">Loading...</div>
       }
     </div>
-  )
+  );
 };
 
 export default ItemData;

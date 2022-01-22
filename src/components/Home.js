c62b65ae2003aa88e328a805df68ba29d880cc8b
Brandon Lim <brandonlimchi@gmail.com>
2022-01-22 07:55:07 -0800

    implement initial Home page component

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+const Home = () => {
+  return (
+    <main id="home">
+      <h1 className="market-mog-header">Market Mog</h1>
+      <SearchBar />
+      <div className="home-welcome">
+        <p>
+          Pick a world and search an item to get its sales data within the past week! <br />
+          Check out the prices of a slice of (not so) tasty
+          <a 
+            className="sample-item" 
+            href="/item/36036/balmung"
+          > Archon Loaf </a>
+          to try it out!
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Home;

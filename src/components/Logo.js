76af55f94d3725ca6b018f788bd4ac85092e2556
Brandon Lim <brandonlimchi@gmail.com>
2022-01-23 04:40:31 -0800

    change link to a tag

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,15 @@
 import React from "react";
 import LogoImage from "../assets/logo512.png";
-import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="/">
+    <a href="/">
       <img 
         className="market-mog-logo" 
         src={LogoImage} 
         alt="Market Mog Logo" 
       />
-    </Link>
+    </a>
   );
 };
 

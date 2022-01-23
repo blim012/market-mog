aee1fe97c0ec7fa66f91f7dd82807058b0cd15e6
Brandon Lim <brandonlimchi@gmail.com>
2022-01-23 01:16:15 -0800

    implement credits component

---
+++ src/components/Credits.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Credits = () => {
+  return (
+    <footer className="credits">
+      <p>
+        ALL FINAL FANTASY XIV CONTENT IS PROPERTY OF SQUARE ENIX CO., LTD <br></br>
+        Created using the 
+        <a href="https://universalis.app/docs/index.html" target="_blank"> Universalis API </a>
+        and
+        <a href="https://xivapi.com/" target="_blank"> xivapi</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Credits;
